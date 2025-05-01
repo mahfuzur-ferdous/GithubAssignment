048b5a477d4d2e86f6003303439e9fbcdfaa2412
mahfuzur-ferdous <mahfuzur.rahmanferdous@brainstation-23.com>
2025-05-01 23:40:23 +0600

    Adding Factorial Problem

--- basic_program.cpp
+++ basic_program.cpp
@@ -4,4 +4,20 @@ int main()
 {
     cout<<"Hello World";
     return 0;
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n < 0)
+        cout << "Error! Factorial of a negative number doesn't exist.";
+    else {
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " = " << factorial;    
+    }
+
+    return 0;
 }
