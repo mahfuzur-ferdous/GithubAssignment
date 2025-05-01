ca3a3005b5832d10f57766e33aa64b5f8107992a
mahfuzur-ferdous <mahfuzur.rahmanferdous@brainstation-23.com>
2025-05-02 01:03:17 +0600

    Fibonacci Added

--- basic_program.cpp
+++ basic_program.cpp
@@ -15,8 +15,21 @@ int main()
         for(int i = 1; i <= n; ++i) {
             factorial *= i;
         }
-        cout << "Factorial of " << n << " = " << factorial;    
+        cout << "Factorial of " << n << " = " << factorial<<endl;   
     }
 
+    if (n < 0)
+        cout << "Error! Fibonacci of a negative number doesn't exist." << endl;
+    else {
+        int a = 0, b = 1, next;
+        cout << "Fibonacci sequence up to " << n << ": ";
+        for (int i = 0; i < n; ++i) {
+            cout << a << " ";
+            next = a + b;
+            a = b;
+            b = next;
+        }
+        cout << endl;
+    }
     return 0;
 }
